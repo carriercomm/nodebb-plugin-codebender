e1f2177f9fd634950a63e49d8ffa94a3c31cc394
Henrik Ekblad <henrik.ekblad@gmail.com>
2014-08-19 09:28:27 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# NodeBB Codebender Plugin
+
+This NodeBB plugin allows users to embed Arduino code using Codebender IDE in their posts. 
+The plugin picks up urls like this https://codebender.cc/example/03.Analog/Calibration in posts and creates a corrects codebender iframe embed tag.
+
+## Installation
+
+    npm install nodebb-plugin-codebender
+
+## Screenshots
+
---
+++ library.js
@@ -0,0 +1,21 @@
+(function(module) {
+	"use strict";
+	var Codebender = {},
+		embed = '<div class="codebender-container"><iframe class="codebender-plugin" src="//www.codebender.com/embed/$1" allowTransparency="true" frameborder="0"></iframe></div>';
+
+	Codebender.parse = function(postContent, callback) {
+		var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:codebender\.cc)\/(.+)">.+<\/a>/g;
+		var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)codebender.cc\/embed\/([\w\-_]+)">.+<\/a>/;
+
+		if (postContent.match(embedUrl)) {
+			postContent = postContent.replace(embedUrl, embed);
+		}
+		if (postContent.match(regularUrl)) {
+			postContent = postContent.replace(regularUrl, embed);
+		}
+
+		callback(null, postContent);
+	};
+
+	module.exports = Codebender;
+}(module));
