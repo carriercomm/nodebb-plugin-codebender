525b60ddc174d6804ee19e5b3a89de39ac7e47ae
Henrik Ekblad <henrik.ekblad@gmail.com>
2014-08-19 10:05:26 +0200

    fix url

--- library.js
+++ library.js
@@ -1,11 +1,11 @@
 (function(module) {
 	"use strict";
 	var Codebender = {},
-		embed = '<div class="codebender-container"><iframe class="codebender-plugin" src="//www.codebender.com/embed/$1" allowTransparency="true" frameborder="0"></iframe></div>';
+		embed = '<div class="codebender-container"><iframe class="codebender-plugin" src="https://codebender.cc/embed/$1" allowTransparency="true" frameborder="0"></iframe></div>';
 
 	Codebender.parse = function(postContent, callback) {
 		var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:codebender\.cc)\/(.+)">.+<\/a>/g;
-		var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)codebender.cc\/embed\/([\w\-_]+)">.+<\/a>/;
+		var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)codebender\.cc\/embed\/([\w\-_]+)">.+<\/a>/;
 
 		if (postContent.match(embedUrl)) {
 			postContent = postContent.replace(embedUrl, embed);
