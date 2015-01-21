14f769a6aaa244d8675fa7422f8d5a77d76069a4
Henrik Ekblad <henrik.ekblad@gmail.com>
2015-01-21 19:47:41 +0100

    Update plugin to be compatible with nodebb 0.6.0

--- library.js
+++ library.js
@@ -3,18 +3,20 @@
 	var Codebender = {},
 		embed = '<iframe class="codebender-plugin" src="https://codebender.cc/embed/$1?referrer=MySensors" allowTransparency="true" frameborder="0"></iframe>';
 
-	Codebender.parse = function(postContent, callback) {
+	Codebender.parse = function(data, callback) {
 		var	regularUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:codebender\.cc)\/(.+)">.+<\/a>/g;
 		var	embedUrl = /<a href="(?:https?:\/\/)?(?:www\.)codebender\.cc\/embed\/([\w\-_]+)">.+<\/a>/;
 
-		if (postContent.match(embedUrl)) {
-			postContent = postContent.replace(embedUrl, embed);
-		}
-		if (postContent.match(regularUrl)) {
-			postContent = postContent.replace(regularUrl, embed);
+		if (data && data.postData && data.postData.content) {	
+			if (data.postData.content.match(embedUrl)) {
+				data.postData.content = data.postData.content.replace(embedUrl, embed);
+			}
+			if (data.postData.content.match(regularUrl)) {
+				data.postData.content = data.postData.content.replace(regularUrl, embed);
+			}
 		}
 
-		callback(null, postContent);
+		callback(null, data);
 	};
 
 	module.exports = Codebender;
